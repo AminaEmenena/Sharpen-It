387139d40fb6539ab4011d4600a5bb8a6e5bd545
AminaEmenena <34988195+AminaEmenena@users.noreply.github.com>
2021-03-08 20:06:56 -0800

    Initial Files from Examples

---
+++ HelloWorld_Udemy/HelloWorld_Udemy/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld_Udemy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I should be good at this by now!");
+            
+        }
+    }
+}
